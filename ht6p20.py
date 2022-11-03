6474adbed0ecd3dcc173624c40ca462973b34525
projetoarduino <projetoarduino@gmail.com>
2022-11-03 09:44:20 -0300

    work but is not usefull, but code use all poer of esp32

--- ht6p20.py
+++ ht6p20.py
@@ -1,11 +1,8 @@
-# based on https://acturcato.wordpress.com/2013/12/20/decodificador-para-o-encoder-ht6p20b-em-arduino/
-# and direct converted from arduino code
+from machine import Pin, time_pulse_us
 
-from machine import Pin
-import time
 
-class In:
-    def __init__(self, pin1):
+class Start:
+    def __init__(self, pinNumber):
         self.result = None
         self.start_dur0 = 0
         self.dur0 = 0
@@ -16,68 +13,53 @@ class In:
         self.counter = 0
         self.startbit = False
         self.addr = 0
-        if pin1 is not None:
-            self.pin = Pin(pin1, Pin.IN, pull = Pin.PULL_DOWN) #before Pin.PULL_DOWN
-            #self.pin.callback(Pin.IRQ_RISING | Pin.IRQ_FALLING, self.trig)
-            self.pin.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.trig)
+        self.data_return = ""
+        self.pin = Pin(pinNumber, Pin.IN, pull=Pin.PULL_DOWN)
 
-    def trig(self, pin):
-        if self.pin.value() == 0:
-            self.start_dur0 = time.ticks_us()
-        elif self.start_dur0 is not 0:
-            #self.dur0 = time.ticks_diff(time.ticks_us(), self.start_dur0)
-            self.dur0 = abs(self.start_dur0 - time.ticks_us())
-            self.start_dur0 = 0
+    def readRF(self):
+        if self.startbit == False:
+            # Check the PILOT CODE until START BIT;
+            self.dur0 = time_pulse_us(self.pin, 0, 15000)
 
-        if self.pin.value() == 1:
-            self.start_dur1 = time.ticks_us()
-        elif self.start_dur1 is not 0:
-            #self.dur1 = time.ticks_diff(time.ticks_us(), self.start_dur1
-            self.dur1 = abs(self.start_dur1 - time.ticks_us())
-            self.start_dur1 = 0
-                
-        #If time at "0" is between 9200 us (23 cycles of 400us) and 13800 us (23 cycles of 600 us).
-        if self.dur0 > 9200 and self.dur0 < 13800 and self.startbit == False:
-            #print(self.dur0, self.dur1)
-            #calculate wave length - lambda
-            self.wave_lambda = self.dur0 / 23
-      
-            #Reset variables
-            self.dur0 = 0
-            self.buffer = 0
-            self.counter = 0      
-            self.startbit = True
+            # If time at "0" is between 9200 us (23 cycles of 400us) and 13800 us (23 cycles of 600 us).
+            if self.dur0 > 9200 and self.dur0 < 13800 and self.startbit == False:
+                # calculate wave length - lambda
+                self.wave_lambda = self.dur0 / 23
+                # Reset variables
+                self.dur0 = 0
+                self.buffer = 0
+                self.counter = 0
+                self.startbit = True
 
         if self.startbit == True and self.counter < 28:
             self.counter += 1
-    
-            if self.dur1 > 0.5 * self.wave_lambda and self.dur1 < 1.5 * self.wave_lambda:  #If pulse width at "1" is between "0.5 and 1.5 lambda", means that pulse is only one lambda, so the data é "1".
-                self.buffer = (self.buffer << 1) + 1   # add "1" on data buffer    
-            elif self.dur1 > 1.5 * self.wave_lambda and self.dur1 < 2.5 * self.wave_lambda:  #If pulse width at "1" is between "1.5 and 2.5 lambda", means that pulse is two lambdas, so the data é "0".
-                self.buffer = (self.buffer << 1) + 0    #add "0" on data buffer
+            self.dur1 = time_pulse_us(self.pin, 1, 1000)
+            # If pulse width at "1" is between "0.5 and 1.5 lambda", means that pulse is only one lambda, so the data é "1".
+            if self.dur1 > 0.5 * self.wave_lambda and self.dur1 < (1.5 * self.wave_lambda):
+                self.buffer = (self.buffer << 1) + 1   # add "1" on data buffer
+            # If pulse width at "1" is between "1.5 and 2.5 lambda", means that pulse is two lambdas, so the data é "0".
+            elif self.dur1 > 1.5 * self.wave_lambda and self.dur1 < (2.5 * self.wave_lambda):
+                self.buffer = (self.buffer << 1)  # add "0" on data buffer
             else:
-                #Reset the loop
+                # Reset the loop
                 self.startbit = False
 
-        #Check if all 28 bits were received (22 of Address + 2 of Data + 4 of Anti-Code)
-        if self.counter == 28:
-            print("->", self.buffer, self.buffer >> 6, )
-            print("->", bin(self.buffer))
-            print("->", bin(self.buffer >> 6))
-            self.counter = 0
-            self.startbit = False
-            
-            # #I can't extract the Anti-Code or the data
-            # #Check if Anti-Code is OK (last 4 bits of buffer equal "0101")
-            # if bin(self.buffer)[2] == "1" and bin(self.buffer)[3] == "0" and bin(self.buffer)[4] == "1" and bin(self.buffer)[5] == "0":     
-            #     self.counter = 0
-            #     self.startbit = False
-      
-            #     #Get ADDRESS CODE from Buffer
-            #     self.addr = self.buffer >> 6
-            # else:    
-            #     #Reset the loop
-            
-    
+        # Check if all 28 bits were received (22 of Address + 2 of Data + 4 of Anti-Code)
+        if self.counter == 28:            
+            data = '{0:b}'.format(self.buffer)
+            # Check if Anti-Code is OK (last 4 bits of buffer equal "0101")
+            # Estou compensan o 0b no inicio e acrescentei um verificação de tamanho da string para evitar o out of range 
+            if len(data) == 28 and data[25] == "0" and data[26] == "1" and data[27] == "0":
+                self.counter = 0
+                self.startbit = False
+
+                addr = self.buffer >> 6
+                #print("->", data, addr, data[23], data[24]) #data binary, address, button 1, button 2    
+                self.data_return = '{},{},{},{}'.format(addr, data[23], data[24], data)
+                print(self.data_return)
+            else:
+                # Reset the loop
+                self.startbit = False
+
     def read(self):
-        return self.buffer
+        return self.data_return
--- main1.py
+++ main1.py
@@ -1,11 +1,26 @@
 import ht6p20
 import time
+from power import Power #coment if you not use m5stack core2
+import _thread
+Power()
 
-rf433 = ht6p20.In(36) #interrupt pin
+count = 0
+# not work correct in a tread
+def readSensorsRF():
+    rf433 = ht6p20.Start(36)
+    while True:
+        rf433.readRF()
+        time.sleep_us(1)
+
+
+_thread.start_new_thread(readSensorsRF, ())
 
 while True:
-    code = rf433.read()
-    #print(code)
-    time.sleep_ms(100)
+    count = count + 1
+    print("Running", count)
+    time.sleep_ms(10)
 
-    
+#correct way but not usefull
+# rf433 = ht6p20.Start(36)
+# while True:    
+#     rf433.readRF()
